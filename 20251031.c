a04c1ac715a8c12dad97dfe9612c1a6f41a5bf21
teaft <t246526@st.yamagata-u.ac.jp>
2025-10-31 18:40:01 +0900

    Add files via upload

---
+++ 20251031.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int diff;
+    char large; // 大文字
+    char small; // 小文字
+    
+    printf("アルファベットの小文字を入力してください>>>");
+    scanf("%c",&small);
+    diff='A'-'a';  // 'a'と'A'との文字の差
+    large=small+diff; // 大文字に変換
+    
+    printf("小文字は%c  大文字は%c\n",small,large);
+    
+    return 0;
+    
+    
+}
